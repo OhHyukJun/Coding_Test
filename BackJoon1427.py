31e38c72ee0362f2c28f51fcad1fdec458e3a220
OhHyukJun <ohj3961@gmail.com>
2022-12-31 20:24:02 +0900

    initial commit

--- BackJoon1427.py
+++ BackJoon1427.py
@@ -1,7 +1,16 @@
 import sys
-out = sys.stdout.readline
-#값을 출력할 때 사용
-
+out = sys.stdout.write #값을 출력할 때 사용
 A = list(input())
+N = len(A)
 
-for i in range(N)
+for i in range(N):
+    Max = i
+    for j in range(i+1,N):
+        if A[j]>A[Max]: #오름차순일 경우에는 최솟값을 찾는다
+            Max=j 
+    if A[i] < A[Max]:
+        temp = A[i]
+        A[i] = A[Max]
+        A[Max] = temp # 두 값을 바꿔준다
+for i in range(N):
+    out(A[i])
